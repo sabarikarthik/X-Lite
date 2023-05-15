d866eded96f98eb30065226e00a74f42b7f5d5ee
sabarikarthik <sabarikarthik1029@gmail.com>
2023-05-15 06:25:57 +0530

    Backend Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# X-Lite 
+Current Version: 1.0
+Supports authentication, follow users and get their posts, creating and liking posts, user suggestions to follow..
---
+++ backend/routes/post.route.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+	commentOnPost,
+	replyOncomment,
+	createPost,
+	deletePost,
+	getAllPosts,
+	getFollowingPosts,
+	getLikedPosts,
+	getUserPosts,
+	likeUnlikePost,
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/reply/:id", protectRoute, replyOncomment);
+router.delete("/:id", protectRoute, deletePost);
+
+export default router;
